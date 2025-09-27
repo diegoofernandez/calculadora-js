22b1866def4229665951996ac1dee906aa8a884d
Diego Fernández <diego225587@gmail.com>
2025-09-26 23:45:27 -0300

    mejorando tokenizacion pre shuntingyard

--- src/engine/parser/Shuntingyard.ts
+++ src/engine/parser/Shuntingyard.ts
@@ -4,6 +4,7 @@ export default class Parser{
     protected posfixConversion: string[] = [];
     protected pila: string[] = [];
     protected cadena: string; 
+    protected bufferElem: string[] = []; 
 
     private enteros = /^\d+$/; 
 
@@ -14,29 +15,66 @@ export default class Parser{
         "*": 2,
         "/": 2
     }; 
+
     protected context = "|"; 
     protected objetoMatematico = ["frac", "expo", "eculin", "ecurac", "pol"]; 
 
     constructor(data: string, road: number){
         this.cadena = data; 
-        this.tokenization(this.cadena); 
         if(road == 1){
             //objetos complejos
+            this.tokenization(this.cadena); 
         }else{
+            this.tokenizacionElem(this.cadena);
             this.conversionElem(); 
         }
     }
 
-    //tokenizacion de la cadena
+    //tokenizacion de las cadenas
     protected tokenization(cadena: string){
         this.infija = cadena.split("");
+    }
+    protected tokenizacionElem(cadena: string){
+        let i = 0; 
+
+        while(i < cadena.length){
+
+            let digito = cadena[i]; 
+
+            if(digito === ' '){
+                i++; 
+                continue; 
+            } 
+
+            if(this.enteros.test(digito)){
+
+                let numeroExtenso = ''; 
+                while(i < cadena.length && this.enteros.test(cadena[i])){
+                    numeroExtenso+=cadena[i]; 
+                    i++;
+                }
+                this.infija.push(numeroExtenso);
+                continue;
+
+            }
+
+            if(['+', '-', '*', '/', '(', ')'].includes(digito)){
+                this.infija.push(digito); 
+                i++;
+                continue; 
+            }
+
+
+        }
+
     }
 
     //obtenemos todos los digitos de un entero de más de 1 digito. Ej: 3889
     protected tokenizNumeros(posicionAcual: number): any{
         if(this.enteros.test(this.infija[posicionAcual])){
-            return 1 + this.tokenizNumeros(posicionAcual +1)
+            return 1 + this.tokenizNumeros(posicionAcual +1);
         }
+        return console.log("Digitos recuperados"); 
     }
 
     //SHUNTHING-YARD ALGORITMO
@@ -46,16 +84,9 @@ export default class Parser{
 
             //situaciones básicas de inserción a pila y posfija
             if(this.enteros.test(this.infija[i])){
+ 
+                this.posfixConversion.push(this.infija[i]); //agregamos si es un número
 
-                let actualPosicion = parseInt(this.infija[i]);
-                let retornoNumeroCompleto = this.tokenizNumeros(i); 
-                let tempArray = this.infija.splice(actualPosicion, retornoNumeroCompleto);
-                let tempNumber = ""; 
-
-                for(let x = 0; tempArray.length; x ++){//el for recorrerá todo el número entero
-                    tempNumber = tempNumber + tempArray[x];
-                }
-                this.posfixConversion.push(tempNumber); //agregamos si es un número
             
             }else if(this.infija[i] == "("){
 
