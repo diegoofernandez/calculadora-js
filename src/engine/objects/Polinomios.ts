bfc3a40644a0de659a839e65fb12f23d503c1a71
Diego Fernández <diego225587@gmail.com>
2025-10-13 23:12:51 -0300

    refactorizacion grobner

--- src/engine/objects/Polinomios.ts
+++ src/engine/objects/Polinomios.ts
@@ -1,8 +1,4 @@
 import { Euclides } from "../utils/Euclides";
-import Termino from './Termino'; 
-import MCMTerminos from "./MCMTerminos";
-import OperadorTermino from "./OperadorTermino";
-import ComparadorTermino from "./ComparadorTermino";
 
 
 export interface ProcesamientoPolinomio{
@@ -28,95 +24,6 @@ export default class Polinomio{
 
 	}
 
-
-}
-
-export class SPolinomio{
-
-	private listadoPols: Array<{ polinomio: string[], terminoLider: string}> = [];
-	private coeficientes: Termino[] = []; 
-
-	constructor(datos: Array<{ polinomio: string[], terminoLider: string}>, monomios: Termino[]){
-
-		this.listadoPols = datos; 
-		this.coeficientes = monomios; 
-
-	}
-
-	public operacion(){
-
-		let multiplicaciones: Array<{ polinomio: string[], terminoLider: string}> = this.multiplicarCoeficientePorEcuacion(); 
-
-		return multiplicaciones; 
-
-	}
-
-	private multiplicarCoeficientePorEcuacion(): Array<{ polinomio: string[], terminoLider: string}>{
-
-		let tempPolinomio: Array<{ polinomio: string[], terminoLider: string}> = [];
-
-		for (let i = 0; i < this.coeficientes.length; i++) {
-			
-			let resultadoOperaciones: string[] = []; 
-
-			for (let t = 0; t < this.listadoPols[i].polinomio.length; t++) {
-				
-				let multiplicacion = OperadorTermino.multiplicar(this.coeficientes[i], new Termino(this.listadoPols[i].polinomio[t])); 
-				resultadoOperaciones.push(multiplicacion.toString()); 
-
-			}
-
-			let ordenTerminos = OperadorTermino.ordenarPolinomio(resultadoOperaciones);
-
-			resultadoOperaciones.forEach(p => {
-
-				tempPolinomio.push({
-					polinomio: ordenTerminos,
-					terminoLider: ordenTerminos[0]
-				})
-
-			});
-			
-		}
-
-		return this.reducirPolinomios(tempPolinomio);
-
-	}
-
-	private reducirPolinomios(datos: Array<{ polinomio: string[], terminoLider: string}>) {
-
-		let tempPolinomio: string[] = [];  
-
-		for (let i = 0; i < datos[0].polinomio.length; i++) {
-			let termA = new Termino(datos[0].polinomio[i]);
-			let termB = new Termino(datos[1].polinomio[i]);
-			
-			let resultado = OperadorTermino.restar(termA, termB);
-			
-			// Verificar si el resultado NO es cero
-			if (resultado.getCoeficiente() !== 0) {
-				tempPolinomio.push(resultado.toString());
-			}
-		}
-    
-    	// Verificar si el polinomio resultante está vacío (es cero)
-		if (tempPolinomio.length === 0) {
-			
-			return [{
-				polinomio: ["Reducido", "Listo"],
-				terminoLider: 0
-			}];
-			
-		}
-		
-		let ordenado = OperadorTermino.ordenarPolinomio(tempPolinomio); 
-
-		return [{
-			polinomio: ordenado,
-			terminoLider: ordenado[0]
-		}];
-
-	}
 
 }
 
